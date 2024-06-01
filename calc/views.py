da2300948af4878de5b24aea4c902295b95f5a39
nuevo_63 <ajrahuacso@gmail.com>
2024-06-01 17:13:52 -0500

    Se hace otras pruebas con hombre y result y base: Templates

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name':'Navin'});
+
+def add(request):
+    return render(request, 'result.html')
