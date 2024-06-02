9d3cc8a69a63a1e6f86d69e4f982b88e847ebdfc
nuevo_63 <ajrahuacso@gmail.com>
2024-06-02 18:19:15 -0500

    Opcion Listar casi completa, es necesario dar formularios para que funcionen adecuadamente

--- destTuristicos/urls.py
+++ destTuristicos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.destinos , name='destinos')
+    path('listar/', views.destinos , name='destinos')
 ]
--- telusko/urls.py
+++ telusko/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('travello.urls')),
-    path('destinos/', include('destTuristicos.urls')),
+    path('', include('destTuristicos.urls')),
     path('admin/', admin.site.urls),
 ]
 
