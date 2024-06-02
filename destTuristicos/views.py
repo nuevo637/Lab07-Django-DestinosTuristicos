ae91b6e4c70901c37da70cc4c4dd12447b0384eb
nuevo_63 <ajrahuacso@gmail.com>
2024-06-02 18:09:52 -0500

    Se crea la primera opcion de listar, en este caso ya esta hecho el csss y html

--- destTuristicos/views.py
+++ destTuristicos/views.py
@@ -3,8 +3,5 @@ from .models import DestinosTuristicos
 
 # Create your views here.
 def destinos(request):
-    
-    
-    
     destinos = DestinosTuristicos.objects.all()
     return render(request, 'destinos.html', {'destinos': destinos})
