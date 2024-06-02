73593fbadab7cc1999670eaa33b68b12db8613d2
nuevo_63 <ajrahuacso@gmail.com>
2024-06-02 01:00:18 -0500

    se crea el modelo - tabla para el trabajo de laboratorio

--- destTuristicos/models.py
+++ destTuristicos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length = 100)
+    imagenCiudad = models.ImageField(upload_to = 'pics')
+    descripcionCiudad = models.TextField()
+    precioTour = models.IntegerField()
+    ofertaTour = models.BooleanField(default=False)
--- telusko/settings.py
+++ telusko/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'travello.apps.TravelloConfig',
+    'destTuristicos'
 ]
 
 MIDDLEWARE = [
