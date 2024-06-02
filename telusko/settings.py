728d13acf5cb3063ca5d28d6155cd663e620d4fb
nuevo_63 <ajrahuacso@gmail.com>
2024-06-01 23:30:48 -0500

    Se comienzan con la creación de la base de datos

--- telusko/settings.py
+++ telusko/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD': '913397916',
+        'HOST': 'localhost'
     }
 }
 
