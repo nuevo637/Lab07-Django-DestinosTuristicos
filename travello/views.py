16b9feaccf4b096c99724c12b486c94ea15493e9
nuevo_63 <ajrahuacso@gmail.com>
2024-06-01 21:34:16 -0500

    Se crean de manera dinamica los destions

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html');
+
+    dest1  = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The City That Never Sleeps'
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 700
+
+    dest2  = Destination()
+    dest2.name = 'New York'
+    dest2.desc = 'The great City of the World'
+    dest2.img = 'destination_2.jpg'
+    dest2.price = 705
+    
+    dest3  = Destination()
+    dest3.name = 'Havana'
+    dest3.desc = 'The City ost beautifull'
+    dest3.img = 'destination_3.jpg'
+    dest3.price = 708
+    
+    dests = [dest1, dest2, dest3]
+    return render(request, 'index.html', {'dests': dests})
