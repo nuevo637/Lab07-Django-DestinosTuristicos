50ab4478f2fc1c6552257c5b3f6ee15f1c4d69ce
nuevo_63 <ajrahuacso@gmail.com>
2024-06-02 20:27:47 -0500

    Se hacen los 4 templates

--- destTuristicos/urls.py
+++ destTuristicos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('listar/', views.listar_destino , name='listar-destinos'),
-    path('crear/', views.crear_destino , name='crear-destinos')
+    path('crear/', views.crear_destino , name='crear-destinos'),
+    path('editar/', views.editar_destino , name='editar-destino'),
+    path('eliminar/', views.eliminar_destino , name='eliminar-destino')
 ]
--- destTuristicos/views.py
+++ destTuristicos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from .forms import *
 from .models import *
 
@@ -10,9 +11,37 @@ def listar_destino(request):
 #CREAR DESTINOS
 def crear_destino(request):
     if request.method == 'POST':
-        form = DestinosForm(request.POST)
+        form = DestinosForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            return HttpResponse("El destino ha sido creado correctamente") 
     else:
         form = DestinosForm()
     return render(request, 'crear.html', {'form': form})
+
+#EDITAR DESTINO
+def editar_destino(request):
+    destinos = DestinosTuristicos.objects.all()
+    if request.method == 'POST':
+        form = DestinosForm(request.POST, request.FILES)
+        if form.is_valid():
+            nombre_ciudad = form.cleaned_data['nombreCiudad']
+            destino = get_object_or_404(DestinosTuristicos, nombreCiudad=nombre_ciudad)
+            form = DestinosForm(request.POST, instance=destino)
+            if form.is_valid():
+                form.save()
+                return HttpResponse("El destino ha sido guardado correctamente") 
+    else:
+        form = DestinosForm()
+    return render(request, 'editar.html', {'form': form, 'destinos': destinos})
+
+#ELIMINAR DESTINO
+def eliminar_destino(request):
+    if request.method == 'POST':
+        nombre_ciudad = request.POST.get('nombreCiudad')
+        destino = get_object_or_404(DestinosTuristicos, nombreCiudad=nombre_ciudad)
+        destino.delete()
+        return HttpResponse("El destino ha sido eliminado correctamente")  
+    else:
+        destinos = DestinosTuristicos.objects.all()
+        return render(request, 'eliminar.html', {'destinos': destinos})
